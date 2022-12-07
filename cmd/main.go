9fb973853b278995e4c4b2ed0ea80ff24b261ec5
Weslley Ribeiro <weslleypk422@hotmail.com>
2022-12-07 15:08:01 -0300

    adicionando inicializacao do banco de dados

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/WeslleyRibeiro-1999/login-go/database"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	database, err := database.NewDatabase()
+	if err != nil {
+		log.Fatalf("Erro ao inicializar database: %+v", err)
+	}
+	defer database.Close()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "HELLO WORLD!")

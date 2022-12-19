c56c5f1c58b04393fa81ceeadf687b5c12cb2271
Weslley Ribeiro <weslleypk422@hotmail.com>
2022-12-19 18:53:23 -0300

    atualizando model de user

--- models/user.go
+++ models/user.go
@@ -1,6 +1,18 @@
 package models
 
+import "time"
+
 type User struct {
+	ID        int64     `json:"id" gorm:"column:id;primary_key;AUTO_INCREMENT"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserLogin struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }

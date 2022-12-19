0b03362dd69edfaaa0dc43a41e6cbb37a6466b04
Weslley Ribeiro <weslleypk422@hotmail.com>
2022-12-19 18:53:52 -0300

    alterando retorno de inicializacao de database

--- database/database.go
+++ database/database.go
@@ -1,14 +1,12 @@
 package database
 
 import (
-	"database/sql"
-
 	"github.com/WeslleyRibeiro-1999/login-go/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func NewDatabase() (*sql.DB, error) {
+func NewDatabase() (*gorm.DB, error) {
 	dsn := "root:root@tcp(localhost:3306)/user?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -17,10 +15,5 @@ func NewDatabase() (*sql.DB, error) {
 
 	database.AutoMigrate(&models.User{})
 
-	sqlDB, err := database.DB()
-	if err != nil {
-		return nil, err
-	}
-
-	return sqlDB, nil
+	return database, nil
 }

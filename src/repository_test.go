c15806d3de9d93cb9c42951b7684a9f897d88a78
Weslley Ribeiro <weslleypk422@hotmail.com>
2023-01-04 18:46:49 -0300

    add funcao test de login

--- src/repository_test.go
+++ src/repository_test.go
@@ -44,13 +44,57 @@ func TestRepository_SingUp(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			user, err := repository.SingUp(tt.args.user)
-			assert.Nil(t, err, "tem que ser vazio")
+			assert.NoError(t, err, "tem que ser vazio")
 
 			assert.Equal(t, tt.want.ID, user.ID, "devem ser iguais")
 			assert.Equal(t, tt.want.Email, user.Email, "devem ser iguais")
 			assert.Equal(t, tt.want.Password, user.Password, "devem ser iguais")
-
-			repository.db.Delete(models.User{ID: tt.args.user.ID})
 		})
 	}
+}
+
+func TestSignIn(t *testing.T) {
+	db, err := database.NewDatabase()
+	assert.NoError(t, err, "sem erro ao inicializar o banco de dados")
+	repository := NewRepository(db)
+
+	tests := []struct {
+		name string
+		args *models.UserLogin
+		want *models.User
+	}{
+		{
+			name: "deve buscar usuario pelo email e senha",
+			args: &models.UserLogin{
+				Email:    "teste@teste.com",
+				Password: "teste123",
+			},
+			want: &models.User{
+				ID:        20,
+				FirstName: "Weslley",
+				LastName:  "Jose",
+				Email:     "teste@teste.com",
+				Password:  "teste123",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			user, err := repository.SignIn(tt.args)
+			assert.NoError(t, err, "erro ao buscar usuario")
+
+			assert.Equal(t, tt.want.ID, user.ID, "IDs devem ser iguais")
+			assert.Equal(t, tt.want.Email, user.Email, "emails devem ser iguais")
+			assert.Equal(t, tt.want.Password, user.Password)
+		})
+	}
+
+}
+
+func TestDropTable(t *testing.T) {
+	db, err := database.NewDatabase()
+	assert.NoError(t, err, "sem erro ao inicializar o banco de dados")
+	repository := NewRepository(db)
+	repository.db.Migrator().DropTable(&models.User{})
 }

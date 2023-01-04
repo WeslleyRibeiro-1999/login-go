766b7b719515c7e7649c5a2110f33e73edbcf5c9
Weslley Ribeiro <weslleypk422@hotmail.com>
2023-01-04 18:47:10 -0300

    atualizando coluna email para unique

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        int64     `json:"id" gorm:"column:id;primary_key;AUTO_INCREMENT"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
+	Email     string    `json:"email" gorm:"unique"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
